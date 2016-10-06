d310f4b3bb534c48e20c4bf300710a0c3cc5a229
florenceC <florence.choquet@gmail.com>
2016-10-06 14:59:49 -0700

    home page

--- FitBitUI/www/js/app.js
+++ FitBitUI/www/js/app.js
@@ -77,6 +77,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
+  })
+
+  .state('tab.exercise', {
+    url: '/exercise',
+    views: {
+      'exercise': {
+        templateUrl: 'templates/exercise.html',
+        controller: 'DashCtrl'
+      }
+    }
   });
 
   // if none of the above states are matched, use this as the fallback
