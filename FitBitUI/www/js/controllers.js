d0e0d80bbe2037af30b380a1670ae7e96f320d9a
Narith Choeun <narithchoeun@hotmail.com>
2016-11-05 22:34:26 -0700

    added information and goals into account tab

--- FitBitUI/www/js/controllers.js
+++ FitBitUI/www/js/controllers.js
@@ -11,7 +11,51 @@ angular.module('starter.controllers', [])
 .controller('CalendarCtrl', function($scope) {})
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+  $scope.goals = [
+    {
+      "text": "Lose 10 pounds",
+      "checked": true
+    },
+    {
+      "text": "Run 1 Mile",
+      "checked": false
+    },
+    {
+      "text": "Drink 1L of Water",
+      "checked": true
+    }
+  ];
+
+  $scope.checkGoal = function(){
+    // for(int i = 0; i < goals.length; i++){
+    //
+    // }
+  }
+  // $scope.addGoal = function(){
+  //   console.log("user wants to add a goal");
+  //
+  // }
+
+  $scope.infoLabels = [
+    // {
+    //   "title": "First Name",
+    //   "content":""
+    // },
+    // {
+    //   "title": "Last Name",
+    //   "content":""
+    // },
+    {
+      "title": "Age",
+      "content":""
+    },
+    {
+      "title": "Current Weight",
+      "content":""
+    },
+    {
+      "title": "Goal Weight",
+      "content":""
+    }
+  ]
 });
