02b6d60edbe630b49eeaa78ddd465d5d21d33150
djblanc360 <djblanc360@gmail.com>
2016-11-10 14:54:40 -0800

    nutrition halfway there

--- FitBitUI/www/js/controllers.js
+++ FitBitUI/www/js/controllers.js
@@ -1,57 +1,53 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-  $scope.days = [
-    {
-      "day": 'S',
-      "date": 6
-    },
-    {
-      "day": 'M',
-      "date": 7
-    },
-    {
-      "day": 'T',
-      "date": 8
-    },
-    {
-      "day": 'W',
-      "date": 9
-    },
-    {
-      "day": 'T',
-      "date": 10
-    },
-    {
-      "day": 'F',
-      "date": 11
-    },
-    {
-      "day": 'S',
-      "date": 12
-    }
-  ];
-  $scope.type = 'W';
-  $scope.setActive = function(event){
-    $scope.type = angular.element(event.target);
-    console.log($scope.type);
-  };
-})
+.controller('DashCtrl', function($scope) {})
 
-.controller('ExerciseCtrl', function($scope) {})
+.controller('ExerciseCtrl', function($scope) {
+
+})
 
 .controller('HealthCtrl', function($scope) {})
 
+
+
 .controller('NutritionCtrl', function($scope, $timeout) {
-	
+/**	
 	console.log('hi@');
 	angular.element('accordion'); //get elements by class name
+	angular.element('accordion-item'); //get elements by class name
 
 	  $scope.toggleGroup = function(accordion) {
     accordion.show = !accordion.show;
   };
   $scope.isGroupShown = function(accordion) {
     return accordion.show;
+  };
+  */
+  
+
+    $scope.groups = [];
+	for (var i=0; i<4; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: [],
+      show: false
+    };
+
+
+    for (var j=0; j<1; j++) {
+      $scope.groups[i].items.push(i + '-' + j);
+    }
+  }
+  angular.element('group');
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    group.show = !group.show;
+  };
+  $scope.isGroupShown = function(group) {
+    return group.show;
   };
   
   
@@ -72,52 +68,9 @@ angular.module('starter.controllers', [])
 	
 })
 
-.controller('CalendarCtrl', function($scope, $ionicPopup) {
-  $scope.query = "";
-  $scope.friends = [
-    {
-      "name": "Alex",
-      "goal": "Lose 10 lbs"
-    },
-    {
-      "name": "Katarina",
-      "goal": "Lose 10 lbs"
-    },
-    {
-      "name": "Bella",
-      "goal": "Walk 1000 steps"
-    }
-  ];
-
-  $scope.groupGoal = function() {
-
-  }
-
-  $scope.invite = function(){
-
-  }
-
-  $scope.showInviteConfirm = function(text) {
-      var confirmPopup = $ionicPopup.confirm({
-        title: 'Invite a Friend',
-        template: 'Are you sure you want to invite ' + text + '?'
-      });
-
-      confirmPopup.then(function(res) {
-        if(res) {
-          console.log('ok adding');
-          $scope.invite(text);
-          console.log(text);
-        } else {
-          console.log('cancel');
-        }
-      });
-  };
-
-})
+.controller('CalendarCtrl', function($scope) {})
 
 .controller('AccountCtrl', function($scope, $ionicPopup) {
-  $scope.input = "";
   $scope.goal = {
     "text": "",
     "checked": false,
@@ -153,17 +106,13 @@ angular.module('starter.controllers', [])
     if (text === ""){
       console.log("empty input");
     } else {
-      $scope.goals.push({
-        "text": text,
-        "checked": false,
-        "showDelete": false,
-        "showReorder": false
-      });
-      $scope.input = "";
+      $scope.goals.push($scope.goal);
+      // $scope.goal.text = "";
     }
   };
 
   $scope.moveGoal = function(goal, fromIndex, toIndex) {
+    console.log("user wants to move goal");
     $scope.goals.splice(fromIndex, 1);
     $scope.goals.splice(toIndex, 0, goal);
   };
@@ -172,28 +121,20 @@ angular.module('starter.controllers', [])
     $scope.goals = $scope.goals.splice(index, 1);
   };
 
-  $scope.showAddConfirm = function(text) {
-    if (text === ''){
-      $ionicPopup.alert({
-        title: 'Empty text field',
-        template: 'Please input a goal'
-      });
-    } else {
-      var confirmPopup = $ionicPopup.confirm({
-        title: 'Add a Goal',
-        template: 'Are you sure you want to add this goal?'
-      });
+  $scope.showAddConfirm = function(goal) {
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Add a Goal',
+      template: 'Are you sure you want to add this goal?'
+    });
 
-      confirmPopup.then(function(res) {
-        if(res) {
-          console.log('ok adding');
-          $scope.addGoal(text);
-          console.log(text);
-        } else {
-          console.log('cancel');
-        }
-      });
-    }
+    confirmPopup.then(function(res) {
+      if(res) {
+        console.log('ok');
+        $scope.addGoal(goal.text);
+      } else {
+        console.log('cancel');
+      }
+    });
   };
   
   
